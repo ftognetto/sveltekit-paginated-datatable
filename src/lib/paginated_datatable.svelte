4f04595446a47d66fa54dac5ee5036a5a352c1fd
Fabrizio Tognetto <fabriziotognetto@gmail.com>
2023-06-26 12:55:48 +0200

    paginated data reactive

--- src/lib/paginated_datatable.svelte
+++ src/lib/paginated_datatable.svelte
@@ -22,7 +22,7 @@
 	// Exported variables
 	type T = $$Generic;
 	export let paginatedData: PaginatedData<T>;
-
+	$: paginatedData = paginatedData;
 	export let showOptions: PaginatedDatatableShowOptions = new PaginatedDatatableShowOptions();
 	export let columns: { name: string; cssClass?: string }[] = [];
 
